1568b64a281275b219f98a57fc326eec221cf10c
Sooraj Shukla <soorajshukla974@gmail.com>
2021-05-15 22:15:00 +0530

    OTP SERVICE

---
+++ backend/utils/otpService.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const Nexmo = require("nexmo");
+const NEXMO_API_KEY = process.env.NEXMO_API_KEY;
+const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
+const NEXMO_BRAND_NAME = process.env.NEXMO_BRAND_NAME;
+const nexmo = new Nexmo(
+  {
+    apiKey: NEXMO_API_KEY,
+    apiSecret: NEXMO_API_SECRET,
+  },
+  {
+    debug: true,
+  }
+);
+
+const sendOtp = (mobileNumber) => {
+  return new Promise((resolve, reject) => {
+    nexmo.verify.request(
+      {
+        number: mobileNumber,
+        brand: NEXMO_BRAND_NAME,
+        next_event_wait: 60,
+        pin_expiry: 60,
+      },
+      (err, result) => {
+        if (err) {
+          console.log("Error while sending otp");
+          reject(err);
+        } else {
+          resolve(result.request_id); // for verifyRequestId
+        }
+      }
+    );
+  });
+};
+const verifyOtp = (verifyRequestId, otp) => {
+  console.log(verifyRequestId, otp);
+  return new Promise((resolve, reject) => {
+    nexmo.verify.check(
+      {
+        request_id: verifyRequestId,
+        code: otp,
+      },
+      (err, result) => {
+        if (err) {
+          reject("Error while verifying the OTP!");
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
+};
+
+module.exports = {
+  sendOtp,
+  verifyOtp,
+};
