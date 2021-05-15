703633260fd6c829f63efa4e79fb4057c1969829
Sooraj Shukla <soorajshukla974@gmail.com>
2021-05-15 22:18:37 +0530

    connection.js

---
+++ backend/models/connection.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const url = process.env.databaseUrl;
+const dbName = process.env.dbName;
+
+const mongoose = require('mongoose');
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true});
+
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error'));
+db.once('open',function(){
+  console.log('connected');
+});
+
+module.exports = mongoose;
