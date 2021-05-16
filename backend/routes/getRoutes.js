9b2e52463bb41f8e77c8735e7901db7f5c1cf092
Sooraj Shukla <soorajshukla974@gmail.com>
2021-05-16 18:33:17 +0530

    add getRoutes: home, sendVerificationEmail and verifyEmail

--- backend/routes/getRoutes.js
+++ backend/routes/getRoutes.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.route('/home').get(require('../controllers/getControllers/home'));
+router.route('/sendVerificationEmail').get(require('../controllers/getControllers/sendVerificationEmail'));
+router.route('/verifyEmail').get(require('../controllers/getControllers/verifyEmail'))
+
 module.exports = router;
