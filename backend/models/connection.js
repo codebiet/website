aae038b352c9f0e8e5411269cea3d5c4dd0a1924
Sooraj Shukla <soorajshukla974@gmail.com>
2021-05-16 06:09:24 +0530

    modify connection.js

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
-const path = require('path');
 const url = process.env.databaseUrl;
 const dbName = process.env.dbName;
 
 const mongoose = require('mongoose');
-mongoose.connect(path.join(url,dbName),{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true});
 
-db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error'));
 db.once('open',function(){
   console.log('connected');
