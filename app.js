3d4eb755d5790c841e92d8e011753069c6afd8e7
Sooraj Shukla <soorajshukla974@gmail.com>
2021-05-16 19:47:12 +0530

    configure to use ejs

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ app.use(compression());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(__dirname + "/client"));
+app.use(express.static(__dirname + "/client"));
 app.use(logger("dev"));
-// app.set("views", __dirname + "/client/views");
-// app.set("view engine", "ejs");
-// app.engine("html", require("ejs").renderFile);
+app.set("views", __dirname + "/client/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
 
 app.use(
   session({
