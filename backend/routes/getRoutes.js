dd00673a625ecfa471dc71027b7015cfdc036892
Sooraj Shukla <soorajshukla974@gmail.com>
2021-05-16 19:07:27 +0530

    remove bug: change verifyEmailAndPassword to verifyEmailAndPhone route

--- backend/routes/getRoutes.js
+++ backend/routes/getRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/home').get(require('../controllers/getControllers/home'));
-router.route('/verifyEmailAndPassword').get(require('../controllers/getControllers/verifyEmailAndPhone'));
+router.route('/verifyEmailAndPhone').get(require('../controllers/getControllers/verifyEmailAndPhone'));
 router.route('/sendVerificationEmail').get(require('../controllers/getControllers/sendVerificationEmail'));
 router.route('/verifyEmail').get(require('../controllers/getControllers/verifyEmail'))
 
