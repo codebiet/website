2ceeabe54ec8b23aa3444e5812280ad94de82240
Sooraj Shukla <soorajshukla974@gmail.com>
2021-05-19 17:28:52 +0530

    update Navbar: hide navigation hamburger on auth pages

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,30 +1,53 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { AuthContext } from "../state/Store";
 import codeImage from "./assets/codeLogo.png";
-import {logOutUser} from '../state/auth/authActions';
+import { logOutUser } from "../state/auth/authActions";
 import "./styles/Navbar.scss";
 const Navbar = () => {
   const auth = useContext(AuthContext);
+  const location = useLocation();
+  const [showExtraInfoInNavbar, setShowExtraInfoInNavbar] = useState(false);
+  console.log(location.pathname, showExtraInfoInNavbar);
+  useEffect(() => {
+    if (
+      location.pathname != "/login" &&
+      location.pathname != "/register" &&
+      location.pathname != "/setPassword" &&
+      location.pathname != "/forgotPassword" &&
+      location.pathname != "/sentVerifyEmail"
+    ) {
+      setShowExtraInfoInNavbar(true);
+    }
+  }, []);
   const handleLogout = () => {
     logOutUser(auth.dispatch);
-
-  }
+  };
   return (
     <React.Fragment>
       <nav className="nav-bar">
         <img className="code-img" src={codeImage} alt="" />
         <div className="join-us-hamburger-container">
           {auth.state.userLoggedIn ? (
-            <button className="join-us" onClick={()=>handleLogout()}>LOGOUT</button>
-          ) : (
+            <button className="join-us" onClick={() => handleLogout()}>
+              LOGOUT
+            </button>
+          ) : showExtraInfoInNavbar ? (
             <Link to="/register" className="join-us">
               JOIN US
             </Link>
+          ) : (
+            location.pathname == "/regiser" && (
+              <Link to="/login" className="join-us">
+                LOGIN
+              </Link>
+            )
           )}
-          <button className="hamburger-container">
-            <i className="fas fa-bars"></i>
-          </button>
+          {showExtraInfoInNavbar && (
+            <button className="hamburger-container">
+              <i className="fas fa-bars"></i>
+            </button>
+          )}
         </div>
       </nav>
     </React.Fragment>
