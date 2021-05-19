514b1ff55aa96e55dc330be5c2597e311354d2a5
Sooraj Shukla <soorajshukla974@gmail.com>
2021-05-19 18:47:47 +0530

    add post route to set user Password

---
+++ backend/controllers/postControllers/setPassword.js
@@ -0,0 +1,49 @@
+const crypto = require("crypto");
+const User = require("../../models/userModal");
+const bcrypt = require("bcryptjs");
+const decrypt = (cipher) => {
+  const decipher = crypto.createDecipher(
+    process.env.ENCRYPTION_ALGO,
+    process.env.CRYPTOJS_SECRET
+  );
+  var decrypted =
+    decipher.update(cipher, "hex", "utf8") + decipher.final("utf8");
+  return decrypted;
+};
+const changePassword = async (req, res) => {
+  const { password, confirmPassword } = req.body;
+  if (!password || !confirmPassword)
+    return res.status(400).send({ errorMsg: "Both the fields are required!" });
+  //both password and confirmPassword supplied in the body;
+  if (password != confirmPassword)
+    return res.status(401).send({ errorMsg: "Passwords don't match!" });
+  //passwords matched
+  const encryptedID = req.query.id;
+  console.log(encryptedID);
+  //decrypt ID
+  const decryptedID = decrypt(encryptedID);
+  let user;
+  try {
+    user = await User.findById(decryptedID).exec();
+  } catch (err) {
+    return res.status(500).send({ errorMsg: "Internal Server Error" });
+  }
+  if (!user)
+    return res
+      .status(400)
+      .send({ errorMsg: "This link is not valid!" });
+  //valid userId;
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      if (err) res.status(500).send({ errorMsg: "Internal Server Error" });
+      user.password = hash;
+      try {
+        await user.save();
+        return res.send({ msg: "success" });
+      } catch (err) {
+        return res.status(500).send({ errorMsg: "Internal Server Error" });
+      }
+    });
+  });
+};
+module.exports = changePassword;
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -5,5 +5,6 @@ router.route("/register").post(require("../controllers/postControllers/signup"))
 router.route("/login").post(require('../controllers/postControllers/login'));
 router.route('/verifyOtp').post(require('../controllers/postControllers/verifyOtp'));
 router.route('/recoverPassword').post(require("../controllers/postControllers/recoverPassword"));
+router.route('/setPassword').post(require("../controllers/postControllers/setPassword"));
 router.route('/changePassword').post(require("../controllers/postControllers/changePassword"));
 module.exports = router;
