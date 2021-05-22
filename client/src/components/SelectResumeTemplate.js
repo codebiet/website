5e2453f5ef0d3dc2f9285d849356b5b9432cab9e
Sooraj Shukla <soorajshukla974@gmail.com>
2021-05-22 16:05:48 +0530

    Select Resume Template Component

---
+++ client/src/components/SelectResumeTemplate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import template1 from "./assets/template1.png";
+import template2 from "./assets/template2.png";
+import template3 from "./assets/template3.png";
+const Template = ({setTemplate}) => {
+    return <div className="resume-select">
+        <h1>SELECT THE TEMPLATE</h1>
+        <div className="templates">
+            <img src={template1} alt="" onClick={() => setTemplate(1)}/>
+            <img src={template2} alt="" onClick={() => setTemplate(2)}/>
+            <img src={template3} alt="" onClick={() => setTemplate(3)}/>
+        </div>
+    </div>
+}
+export default Template;
