8172b55dff56e4e4804fe97635ad0eb19f28a3d8
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-08 15:13:02 +0530

    use lazy imports in errorPage

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,19 +1,19 @@
 import React, { lazy, Suspense } from "react";
 import error from "../components/assets/error.svg";
 import Loader from "../components/Loader/Loader";
-// const Nav = lazy(() => import("../components/Navbar/Nav"));
-// const Footer = lazy(() => import("../components/Footer/Footer"));
-import Nav from "../components/Navbar/Nav";
-import Footer from "../components/Footer/Footer";
+const Nav = lazy(() => import("../components/Navbar/Nav"));
+const Footer = lazy(() => import("../components/Footer/Footer"));
+// import Nav from "../components/Navbar/Nav";
+// import Footer from "../components/Footer/Footer";
 function Error404() {
   return (
-    <Suspense fallaback={<Loader />}>
+    <Suspense fallback={<Loader />}>
       <div className="App">
         <div>
           <Nav />
           <div className="this">
             <div className="error404">
-              <div className="erro404Inner">
+              <div className="erro404Inner" style={{margin:"2rem auto 4rem auto"}}>
                 <img src={error} alt="404 error"></img>
               </div>
             </div>
