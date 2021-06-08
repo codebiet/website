aade1b6436ca29b86702d13578e1a8893572d379
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-09 00:02:08 +0530

    remove bug due to lazy load, define events data in events also

--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -5,8 +5,59 @@ const Footer = lazy(() => import("../components/Footer/Footer"));
 const EventCard = lazy(() => import("../components/EventCard/EventCard"));
 import { Link } from "react-router-dom";
 import Loader from "../components/Loader/Loader";
-// import eventsData from "../components/EventCard/eventsData";
-const eventsData = lazy(() => import("../components/EventCard/eventsData"));
+import svg from "../components/assets/SVG.svg";
+const eventsData = [
+  {
+      name:'BIET HACK | Ideas meet technology',
+      startsOn:'08:00 PM, 24 MAY 2021',
+      entryFee:'Free',
+      venue:'Online',
+      description:'Run through the wave of codes smoothly and showcase your coding skills to stay ahead amongst your peers.',
+      img:svg,
+      tags:['Coding','Second Year']
+  },{
+      name:'How to crack GATE?',
+      startsOn:'08:00 PM, 24 MAY 2021',
+      entryFee:'Free',
+      venue:'Online',
+      description:'The mentors inspire a lot and encourage us to learn new technology. Happy to get training from them.',
+      img:svg,
+      tags:['Gate','All Year']
+  },{
+      name:'Placement Prep. Talk',
+      startsOn:'08:00 PM, 24 MAY 2021',
+      entryFee:'Free',
+      venue:'Online',
+      description:'Run through the wave of codes smoothly and showcase your coding skills to stay ahead amongst your peers.',
+      img:svg,
+      tags:['Coding','Second Year']
+  },{
+      name:'Workshop on Web Development',
+      startsOn:'08:00 PM, 24 MAY 2021',
+      entryFee:'Free',
+      venue:'Online',
+      description:'Run through the wave of codes smoothly and showcase your coding skills to stay ahead amongst your peers.',
+      img:svg,
+      tags:['Coding','Second Year']
+  },{
+      name:'Create a React App',
+      startsOn:'08:00 PM, 24 MAY 2021',
+      entryFee:'Free',
+      venue:'Online',
+      description:'Run through the wave of codes smoothly and showcase your coding skills to stay ahead amongst your peers.',
+      img:svg,
+      tags:['Coding','Second Year']
+  },{
+      name:'BIET CODEathon',
+      startsOn:'08:00 PM, 24 MAY 2021',
+      entryFee:'Free',
+      venue:'Online',
+      description:'Run through the wave of codes smoothly and showcase your coding skills to stay ahead amongst your peers.',
+      img:svg,
+      tags:['Coding','Second Year']
+  },
+]
+// const eventsData = lazy(() => import("../components/EventCard/eventsData"));
 function Events() {
   return (
     <Suspense fallback={<Loader />}>
