7eca56081d59d44e4fb37c3a877487bbf67baa0a
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-12 22:32:00 +0530

    define event modal

---
+++ backend/models/events.js
@@ -0,0 +1,18 @@
+const mongoose = require("./connection");
+const eventSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  type: { type: String, enum: ["Webinar", "Coding"], required: true },
+  entryFee: { type: String, required: true },
+  startsOn: { type: Date, required: true },
+  endsOn: { type: Date, required: true },
+  duration: { type: String, required: true },
+  venue: { type: String, required: true },
+  shortDescription: { type: String, required: true },
+  tags: [String],
+  details: String,
+  banner: { type: String, required: true },
+  cardImg: { type: String, required: true },
+  registered: [mongoose.Schema.Types.ObjectId],
+});
+const Events = mongoose.model("Events", eventSchema);
+module.exports = Events;
