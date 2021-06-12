d870a55293429b242c7d1bb0ba8d7cecee86665c
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-12 23:47:11 +0530

    remove unwanted dummy events data

--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -7,68 +7,6 @@ import { Link } from "react-router-dom";
 import Loader from "../components/Loader/Loader";
 import svg from "../components/assets/SVG.svg";
 import axios from "axios";
-const eventsData = [
-  {
-    name: "BIET HACK | Ideas meet technology",
-    startsOn: "08:00 PM, 24 MAY 2021",
-    entryFee: "Free",
-    venue: "Online",
-    description:
-      "Run through the wave of codes smoothly and showcase your coding skills to stay ahead amongst your peers.",
-    img: svg,
-    tags: ["Coding", "Second Year"],
-  },
-  {
-    name: "How to crack GATE?",
-    startsOn: "08:00 PM, 24 MAY 2021",
-    entryFee: "Free",
-    venue: "Online",
-    description:
-      "The mentors inspire a lot and encourage us to learn new technology. Happy to get training from them.",
-    img: svg,
-    tags: ["Gate", "All Year"],
-  },
-  {
-    name: "Placement Prep. Talk",
-    startsOn: "08:00 PM, 24 MAY 2021",
-    entryFee: "Free",
-    venue: "Online",
-    description:
-      "Run through the wave of codes smoothly and showcase your coding skills to stay ahead amongst your peers.",
-    img: svg,
-    tags: ["Coding", "Second Year"],
-  },
-  {
-    name: "Workshop on Web Development",
-    startsOn: "08:00 PM, 24 MAY 2021",
-    entryFee: "Free",
-    venue: "Online",
-    description:
-      "Run through the wave of codes smoothly and showcase your coding skills to stay ahead amongst your peers.",
-    img: svg,
-    tags: ["Coding", "Second Year"],
-  },
-  {
-    name: "Create a React App",
-    startsOn: "08:00 PM, 24 MAY 2021",
-    entryFee: "Free",
-    venue: "Online",
-    description:
-      "Run through the wave of codes smoothly and showcase your coding skills to stay ahead amongst your peers.",
-    img: svg,
-    tags: ["Coding", "Second Year"],
-  },
-  {
-    name: "BIET CODEathon",
-    startsOn: "08:00 PM, 24 MAY 2021",
-    entryFee: "Free",
-    venue: "Online",
-    description:
-      "Run through the wave of codes smoothly and showcase your coding skills to stay ahead amongst your peers.",
-    img: svg,
-    tags: ["Coding", "Second Year"],
-  },
-];
 // const eventsData = lazy(() => import("../components/EventCard/eventsData"));
 function Events() {
   const [events, setEvents] = useState([]);
