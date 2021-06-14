d6d94fef928ad84c0c0818ffc84efd96c6b89f72
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-14 20:09:20 +0530

    update events schema

--- backend/models/events.js
+++ backend/models/events.js
@@ -12,7 +12,7 @@ const eventSchema = new mongoose.Schema({
   details: String,
   banner: { type: String, required: true },
   cardImg: { type: String, required: true },
-  registered: {type:[mongoose.Schema.Types.ObjectId],select:false},
+  registered: { type: [{ email: String, name: String }], select: false },
 });
 const Events = mongoose.model("Events", eventSchema);
 module.exports = Events;
