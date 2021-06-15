ddf65ee8b4d26190b58af28c4d1fab9de4b909e9
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-15 19:00:14 +0530

    update event get controller to filter events depending upon startsOn in UTC format

--- backend/controllers/getControllers/events.js
+++ backend/controllers/getControllers/events.js
@@ -1,19 +1,24 @@
 const Events = require("../../models/events");
 
+const getUTCDate = (ISTDateString) => {
+  return new Date(new Date(ISTDateString) - 1000 * 60 * (60 * 5 + 30));
+};
 const events = async (req, res) => {
   const pageOptions = {
     page: parseInt(req.query.page) || 0,
     limit: parseInt(req.query.limit) || 10,
-    startsGt: (req.query.gt && new Date(req.query.gt)) || new Date(2019), //startsOn in  database will always be greater than 2019 year
+    startsGt: (req.query.gt && getUTCDate(req.query.gt)) || new Date(2019), //startsOn in  database will always be greater than 2019 year
     startsLt:
-      (req.query.lt && new Date(req.query.lt)) ||
-      new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 12 * 2), //there will be no event in our db which starts 2 years later from today
+      (req.query.lt && getUTCDate(req.query.lt)) ||
+      new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 12), //there will be no event in our db which starts a year later from today
+    type: (req.query.type && [req.query.type]) || ["Coding", "Webinar"], //if type not specified, return all
   };
-  console.log(pageOptions);
+  //since our time zone is GMT+05:30 and mongodb stores date in UTC format, that's why subtracted - 05:30hours in date to get correct match for gt and lt;
   try {
     const noOfItems = await Events.countDocuments({});
     const evnts = await Events.find({
       startsOn: { $gt: pageOptions.startsGt, $lt: pageOptions.startsLt },
+      type: { $in: pageOptions.type },
     })
       .sort({ startsOn: -1 })
       .skip(pageOptions.page * pageOptions.limit)
