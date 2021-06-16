cac9b18dfb940b376c34b47339213d9610f49172
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-16 09:18:42 +0530

    remove bug: correctly check for registered emails

--- backend/controllers/postControllers/eventRegistration.js
+++ backend/controllers/postControllers/eventRegistration.js
@@ -10,20 +10,20 @@ const register = async (req, res) => {
     return res.status(400).send({ errorMsg: "Invalid Email!" });
   try {
     // const evnts = await Events.findById(id).select("+registered");
-    let event = await Events.find({ _id: id, "registered.email": email });
+    let event = await Events.findOne({ _id: id, "registered.email": email });
     if (event)
       return res
         .status(400)
         .send({ errorMsg: "This user is already registered!" });
-    event = await Events.findById(id).exec();
-    event.registered.push({name,email});
+    event = await Events.findById(id).select("+registered").exec();
+    event.registered.push({ name, email });
     await event.save();
     //this cookie will be used to check whether user is registered for particular event or not, since we are giving registration without login
     res.cookie(id, true, {
-        expires: new Date(event.startsOn),
-        httpOnly: false,
-      });
-    return res.send({msg:"Registered successfully"});
+      expires: new Date(event.startsOn),
+      httpOnly: false,
+    });
+    return res.send({ msg: "Registered successfully" });
   } catch (err) {
     console.log(err);
     return res
