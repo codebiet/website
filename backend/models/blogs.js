17358f92ab9f032c40042ddd4be3c1890e6f3f4a
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-16 12:21:42 +0530

    define Blogs schema

---
+++ backend/models/blogs.js
@@ -0,0 +1,25 @@
+const mongoose = require("./connection");
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  url: {
+    type: String,
+    default: function () {
+      return this.title.split(" ").join("-");
+    },
+  },
+  content: String,
+  discardReason: String,
+  suggestedAt: { type: Date, default: Date.now() },
+  postedAt: { type: Date },
+  pickedBy: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
+  state: {
+    type: String,
+    enum: ["AVAILABLE", "PICKED", "DRAFT", "PENDING", "APPROVED", "DISCARDED"],
+    default: "AVAILABLE",
+  },
+  tags: [String],
+  suggestedBy: { type: String, default: "ADMIN" },
+  approvedSuggestion: { type: Boolean, default: false },
+});
+const Blogs = mongoose.model("Blogs", blogSchema);
+module.exports = Blogs;
