5142c9e1a300ef794c512dff5458be97b41972ba
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-17 16:17:28 +0530

    update dashboard sidebar

--- client/src/components/Dashboard_Profile/DashboardSidebar.js
+++ client/src/components/Dashboard_Profile/DashboardSidebar.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import { Nav } from "reactstrap";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-
+import { Collapse } from "reactstrap";
 var ps;
 
 const routes = [
@@ -35,6 +35,12 @@ const routes = [
 function Sidebar(props) {
   const sidebar = React.useRef();
   const location = useLocation();
+  const [articleSubmenuOpen, setArticleSubmenOpen] = useState(
+    location.pathname == "/articles" || location.pathname == "/suggestions"
+  );
+  const toggleArticleSubmenu = () => {
+    setArticleSubmenOpen((prev) => !prev);
+  };
   return (
     <div
       className="sidebar"
@@ -43,26 +49,71 @@ function Sidebar(props) {
     >
       <div className="logo p-5">
         {!props.profileImg && <AccountCircleIcon style={{ fontSize: "140" }} />}
-        {props.profileImg && <img className="profile-img" src={props.profileImg} alt=""/>}
+        {props.profileImg && (
+          <img className="profile-img" src={props.profileImg} alt="" />
+        )}
       </div>
       <div className="sidebar-wrapper" ref={sidebar}>
         <Nav>
-          {routes.map((route, key) => {
-            return (
-              <li
-                className={location.pathname == route.path && "active"}
-                key={key}
+          <li className={location.pathname == "/dashboard" && "active"}>
+            <NavLink
+              to="/dashboard"
+              className="btn btn-default "
+              activeClassName="active"
+            >
+              <p>EDIT PROFILE</p>
+            </NavLink>
+          </li>
+          <li onClick={toggleArticleSubmenu} className="article-collapse-btn">
+            <p>ARTICLES</p>
+          </li>
+          <Collapse isOpen={articleSubmenuOpen}>
+            <li className={location.pathname == "/articles" && "active"}>
+              <NavLink
+                to="/articles"
+                className="btn btn-default "
+                activeClassName="active"
               >
-                <NavLink
-                  to={route.path}
-                  className="btn btn-default "
-                  activeClassName="active"
-                >
-                  <p>{route.name}</p>
-                </NavLink>
-              </li>
-            );
-          })}
+                <p>Your Articles</p>
+              </NavLink>
+            </li>
+            <li className={location.pathname == "/suggestions" && "active"}>
+              <NavLink
+                to="/suggestions"
+                className="btn btn-default "
+                activeClassName="active"
+              >
+                <p>Suggestions</p>
+              </NavLink>
+            </li>
+          </Collapse>
+          <li className={location.pathname == "/user-projects" && "active"}>
+            <NavLink
+              to="/user-projects"
+              className="btn btn-default "
+              activeClassName="active"
+            >
+              <p>PROJECTS</p>
+            </NavLink>
+          </li>
+          <li className={location.pathname == "/competitions" && "active"}>
+            <NavLink
+              to="/competitions"
+              className="btn btn-default "
+              activeClassName="active"
+            >
+              <p>COMPETITIONS</p>
+            </NavLink>
+          </li>
+          <li className={location.pathname == "/logout" && "active"}>
+            <NavLink
+              to="/logout"
+              className="btn btn-default "
+              activeClassName="active"
+            >
+              <p>LOGOUT</p>
+            </NavLink>
+          </li>
         </Nav>
       </div>
     </div>
