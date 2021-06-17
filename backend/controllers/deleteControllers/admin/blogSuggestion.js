aef7514f7482aac395c9e2986d5cd13ff81808d0
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-17 12:09:11 +0530

    apply more sophiisticated filters in getting suggestion and pagination at backend

--- backend/controllers/deleteControllers/admin/blogSuggestion.js
+++ backend/controllers/deleteControllers/admin/blogSuggestion.js
@@ -1,13 +1,13 @@
 const Blogs = require("../../../models/blogs");
 module.exports = async (req, res) => {
+  const getUTCDate = (ISTDateString) => {
+    return new Date(new Date(ISTDateString) - 1000 * 60 * (60 * 5 + 30));
+  };
   const filters = {
     state: (req.query.state && [req.query.state]) || [
       "AVAILABLE",
       "PICKED",
       "DRAFT",
-      "PENDING",
-      "APPROVED",
-      "DISCARDED",
     ],
     approvedSuggestion: (req.query.approvedSuggestion && [
       req.query.approvedSuggestion == "true" || false,
@@ -15,16 +15,36 @@ module.exports = async (req, res) => {
     disapprovedSuggestion: (req.query.disapprovedSuggestion && [
       req.query.disapprovedSuggestion == "true" || false,
     ]) || [true, false],
+    suggestedBy: (req.query.suggestedBy && [req.query.suggestedBy]) || [
+      "ADMIN",
+      "USER",
+    ],
+    suggestedAtGt: (req.query.gt && getUTCDate(req.query.gt)) || new Date(2019), //suggestedAt in database will always be after year 2019
+    suggestedAtLt:
+      (req.query.lt && getUTCDate(req.query.lt)) ||
+      new Date(Date.now() + 1000 * 60 * 60), //there will be no suggestion in our db which we get in future(after current time).
+    page: parseInt(req.query.page) || 0,
+    limit: parseInt(req.query.limit) || 12,
   };
   const id = req.params.id;
   try {
     await Blogs.findOneAndDelete({ _id: id, state: "AVAILABLE" }).exec();
-    const suggestions = await Blogs.find({
+    const totalItems = await Blogs.countDocuments({
+      suggestedAt: { $gt: filters.suggestedAtGt, $lt: filters.suggestedAtLt },
       state: { $in: filters.state },
       approvedSuggestion: { $in: filters.approvedSuggestion },
       disapprovedSuggestion: { $in: filters.disapprovedSuggestion },
-    }).sort({ suggestedAt: -1 }); //getting suggestions;
-    return res.send({ suggestions });
+    });
+    const suggestions = await Blogs.find({
+      suggestedAt: { $gt: filters.suggestedAtGt, $lt: filters.suggestedAtLt },
+      state: { $in: filters.state },
+      approvedSuggestion: { $in: filters.approvedSuggestion },
+      disapprovedSuggestion: { $in: filters.disapprovedSuggestion },
+    })
+      .sort({ suggestedAt: -1 })
+      .skip(filters.page * filters.limit) //pagination starts from 0
+      .limit(filters.limit); //getting suggestions;
+    return res.send({ totalItems, suggestions });
   } catch (err) {
     console.log(err);
     return res
--- backend/controllers/patchControllers/admin/blogSuggestion.js
+++ backend/controllers/patchControllers/admin/blogSuggestion.js
@@ -23,14 +23,14 @@ const getParams = (file) => {
   };
 };
 module.exports = async (req, res) => {
+  const getUTCDate = (ISTDateString) => {
+    return new Date(new Date(ISTDateString) - 1000 * 60 * (60 * 5 + 30));
+  };
   const filters = {
     state: (req.query.state && [req.query.state]) || [
       "AVAILABLE",
       "PICKED",
       "DRAFT",
-      "PENDING",
-      "APPROVED",
-      "DISCARDED",
     ],
     approvedSuggestion: (req.query.approvedSuggestion && [
       req.query.approvedSuggestion == "true" || false,
@@ -38,6 +38,16 @@ module.exports = async (req, res) => {
     disapprovedSuggestion: (req.query.disapprovedSuggestion && [
       req.query.disapprovedSuggestion == "true" || false,
     ]) || [true, false],
+    suggestedBy: (req.query.suggestedBy && [req.query.suggestedBy]) || [
+      "ADMIN",
+      "USER",
+    ],
+    suggestedAtGt: (req.query.gt && getUTCDate(req.query.gt)) || new Date(2019), //suggestedAt in database will always be after year 2019
+    suggestedAtLt:
+      (req.query.lt && getUTCDate(req.query.lt)) ||
+      new Date(Date.now() + 1000 * 60 * 60), //there will be no suggestion in our db which we get in future(after current time).
+    page: parseInt(req.query.page) || 0,
+    limit: parseInt(req.query.limit) || 12,
   };
   const title = req.body.title;
   const tags = JSON.parse(req.body.tags);
@@ -73,12 +83,22 @@ module.exports = async (req, res) => {
   }
   try {
     await suggestion.save();
-    const suggestions = await Blogs.find({
+    const totalItems = await Blogs.countDocuments({
+      suggestedAt: { $gt: filters.suggestedAtGt, $lt: filters.suggestedAtLt },
       state: { $in: filters.state },
       approvedSuggestion: { $in: filters.approvedSuggestion },
       disapprovedSuggestion: { $in: filters.disapprovedSuggestion },
-    }).sort({ suggestedAt: -1 }); //getting suggestions;
-    return res.send({ suggestions: suggestions });
+    });
+    const suggestions = await Blogs.find({
+      suggestedAt: { $gt: filters.suggestedAtGt, $lt: filters.suggestedAtLt },
+      state: { $in: filters.state },
+      approvedSuggestion: { $in: filters.approvedSuggestion },
+      disapprovedSuggestion: { $in: filters.disapprovedSuggestion },
+    })
+      .sort({ suggestedAt: -1 })
+      .skip(filters.page * filters.limit) //pagination starts from 0
+      .limit(filters.limit); //getting suggestions;
+    return res.send({ totalItems, suggestions });
   } catch (err) {
     console.log(err);
     return res
