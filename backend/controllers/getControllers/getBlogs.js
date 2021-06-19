e3d01ab0aa70492300d41bb7492cc70bffa44b8e
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-19 10:42:47 +0530

    update getblogs controller

--- backend/controllers/getControllers/getBlogs.js
+++ backend/controllers/getControllers/getBlogs.js
@@ -17,24 +17,25 @@ module.exports = async (req, res) => {
     page: req.query.page || 0,
     limit: req.query.limit || 10000,
   };
+  if (req.query.writing == "true") filters.state.push("AVAILABLE");//if getting blog detail at write-article page, then blog state will be AVAILABLE
   let dbFilters = {
     postedAt: { $gt: filters.postedAtGt, $lt: filters.postedAtLt },
     state: { $in: filters.state },
   };
   if (req.params.id) dbFilters._id = req.params.id;
-  if (req.params.postedBy) dbFilters.postedBy = req.query.postedBy;
-  //if no filters applied will get all the blogs, in descending order by posted date and time, but will limit to get 1000 blogs if no pagination limit given
-  try {
-    const totalItems = await Blogs.countDocuments(dbFilters);
-    const blogs = await Blogs.find(dbFilters)
-      .sort({ postedAt: -1 })
-      .skip(filters.page * filters.limit) //pagination starts from 0
-      .limit(filters.limit); //getting blogs;
-    return res.send({ totalItems, blogs });
-  } catch (err) {
-    console.log(err);
-    return res
-      .status(500)
-      .send({ errorMsg: "Status-Code: 500, Internal Server Error!" });
-  }
+  if (req.query.postedBy) dbFilters.postedBy = req.query.postedBy;
+    //if no filters applied will get all the blogs, in descending order by posted date and time, but will limit to get 1000 blogs if no pagination limit given
+    try {
+      const totalItems = await Blogs.countDocuments(dbFilters);
+      const blogs = await Blogs.find(dbFilters)
+        .sort({ postedAt: -1 })
+        .skip(filters.page * filters.limit) //pagination starts from 0
+        .limit(filters.limit); //getting blogs;
+      return res.send({ totalItems, blogs });
+    } catch (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .send({ errorMsg: "Status-Code: 500, Internal Server Error!" });
+    }
 };
