eaa2f137b549b7b91db9e7775f86c58cca0e5d23
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-19 14:34:38 +0530

    complete blog writing page

--- client/src/components/WriteArticle/WriteArticle.js
+++ client/src/components/WriteArticle/WriteArticle.js
@@ -2,22 +2,83 @@ import React, { useState, useEffect, useContext } from "react";
 import Loader from "../Loader/Loader";
 import { AuthContext } from "../../state/Store";
 import Editor from "../Editor/Editor";
-import draftjsToHtml from "draftjs-to-html";
-import { Container } from "reactstrap";
+import { Container, Button } from "reactstrap";
 import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
 import { Redirect } from "react-router-dom";
 import Nav from "../Navbar/Nav";
 import Footer from "../Footer/Footer";
+import axios from "axios";
 export default (props) => {
   const auth = useContext(AuthContext);
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const [blog, setBlog] = useState({});
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`/api/blogs/${props.match.params.id}?writing=true`)
+      .then((res) => {
+        setLoading(false);
+        setBlog(res.data.blogs[0]);
+        if (res.data.blogs.length > 0 && res.data.blogs[0].content)
+          setEditorState(
+            EditorState.createWithContent(
+              convertFromRaw(JSON.parse(res.data.blogs[0].content))
+            )
+          );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const getContent = () => {
+    const data = new FormData();
+    data.append(
+      "content",
+      JSON.stringify(convertToRaw(editorState.getCurrentContent()))
+    );
+    return data;
+  };
+  const submitForReview = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios
+      .patch(`/patch/blogs/${props.match.params.id}/submitForReview`, getContent())
+      .then((res) => {
+        setLoading(false);
+        props.history.push("/user-articles");
+      })
+      .catch((err) => {});
+  };
+  const saveAsDraft = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios
+      .patch(`/patch/blogs/${props.match.params.id}/saveAsDraft`, getContent())
+      .then((res) => {
+        setLoading(false);
+      })
+      .catch((err) => {});
+  };
   return (
     <>
       {auth.state.userLoggedIn && (
         <>
           <Nav />
-          <Container style={{margin:"4rem auto"}} className="write-article-container">
+          <Container
+            style={{ margin: "4rem auto" }}
+            className="write-article-container"
+          >
+            <h2 className={loading ? "loading" : ""}>
+              {!loading && blog.title}
+            </h2>
             <Editor editorState={editorState} setEditorState={setEditorState} />
+            <div className="button-container">
+              <Button onClick={(e) => submitForReview(e)}>
+                Submit for Review
+              </Button>
+              <Button onClick={(e) => saveAsDraft(e)}>Save As Draft</Button>
+            </div>
           </Container>
           <Footer />
         </>
@@ -30,6 +91,7 @@ export default (props) => {
           }}
         />
       )}
+      {loading && <Loader />}
     </>
   );
 };
