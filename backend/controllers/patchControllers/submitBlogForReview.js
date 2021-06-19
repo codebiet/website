8fb4a2174e31c957c13b486e461e13467f25e6dd
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-19 20:21:12 +0530

    update submitBlogForReview controller to when blog was posted for review

--- backend/controllers/patchControllers/submitBlogForReview.js
+++ backend/controllers/patchControllers/submitBlogForReview.js
@@ -3,11 +3,19 @@ const Blogs = require("../../models/blogs");
 module.exports = async (req, res) => {
   const { content } = req.body;
   const id = req.params.id;
-  try{
-      await Blogs.findByIdAndUpdate(id,{$set:{content:content,postedAt:new Date(),state:"PENDING",postedBy:req.body.userId}});
-      return res.send({msg:"success"});
-  }catch(err){
-      console.log(err);
-      return res.status(500).send({errorMsg:"Internal Server Error!"});
+  try {
+    await Blogs.findByIdAndUpdate(id, {
+      $set: {
+        content: content,
+        postedAt: new Date(),
+        state: "PENDING",
+        postedBy: req.body.userId,
+        postedAt: new Date(),
+      },
+    });
+    return res.send({ msg: "success" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ errorMsg: "Internal Server Error!" });
   }
 };
