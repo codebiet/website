ecd30b9555eefa48450283e8f5f7c7ad21e03c77
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-19 14:33:59 +0530

    make loader position fixed

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -1,17 +1,7 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 function Loader({fullPageLoader}) {
-    useEffect(() => {
-        window.scrollTo(0,0);
-        const clipBody = () => {
-           document.getElementById('doc-body-main').classList.add('clip-body');
-        }
-        clipBody();
-        return () => {
-            document.getElementById('doc-body-main').classList.remove('clip-body');
-        }
-    })
     return (
-        <div className={"loader-container position-absolute full-width flex " + ((fullPageLoader && "fullPageLoader") || "")}>
+        <div className={"loader-container position-fixed full-width flex " + ((fullPageLoader && "fullPageLoader") || "")}>
             <div className="loader"></div>
         </div>
     )
