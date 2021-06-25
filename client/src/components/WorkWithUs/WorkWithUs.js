398d3cc8c3e43cc68bb570ed9caa56b2918f9678
Sooraj Shukla <soorajshukla974@gmail.com>
2021-06-25 16:42:03 +0530

    link workWithUs button to careers page and update content

--- client/src/components/WorkWithUs/WorkWithUs.js
+++ client/src/components/WorkWithUs/WorkWithUs.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 function WorkWithUs() {
   return (
     <div className="work-with-us-container">
       <div className="work_with_us">
-        <h2>Lorem ipsum dolor sit amet consectetur</h2>
-        <p>Lorem ipsum dolor sit amet consectetur </p>
-        <button className="transitionbtn">Work with us</button>
+        <h2>Alone we can do so little, together we can do so much.</h2>
+        <p>Come and start your journey with CODE.</p>
+        <button className="transitionbtn">
+          <Link to="/careers">Work with us</Link>
+        </button>
       </div>
     </div>
   );
