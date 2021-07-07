9fe9e6cfb412a8c1b7aa53de586344e3a82b7069
Sooraj Shukla <soorajshukla974@gmail.com>
2021-07-07 17:54:21 +0530

    Add BietGems in Nav

--- client/src/components/Navbar/Nav.js
+++ client/src/components/Navbar/Nav.js
@@ -210,6 +210,9 @@ function Nav() {
             <li>
               <NavLink to="/about">About Us</NavLink>
             </li>
+            <li>
+              <NavLink to="/bietGems">BIET Gems</NavLink>
+            </li>
             <li style={{ marginRight: "1rem" }}>
               <NavLink to="/contact">Contact Us</NavLink>
             </li>
