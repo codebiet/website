3596f102171e936f1b9ac32c180fb626e49c35d6
Sooraj Shukla <soorajshukla974@gmail.com>
2021-07-07 18:07:39 +0530

    update Team section 's data at homepage, lazy load images

--- client/src/components/Home/Team.js
+++ client/src/components/Home/Team.js
@@ -4,42 +4,48 @@ import AOS from "aos";
 import Ritesh from "../assets/final Year/ritesh_rai_sir_1.jpg";
 import Samarth from "../assets/final Year/Samarth_Agarwal_Sir.jpg";
 import Rajat from "../assets/final Year/Rajat Saxena.jpg";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 const TeamMemberData = [
   {
     name: "Ritesh Rai",
-    description:
-      "This is basic card with image on top, title, description and button.",
+    description: "Founding Member, CODE",
     img: Ritesh,
     detailedText:
-      "This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.",
+      "He is Computer Science and Engineering branch 2021 batch passout. He holds a command on coding and is also a great Web developer. ",
     fbLink: "https://www.facebook.com/profile.php?id=100023781995286",
     lnkdinLink: "https://www.linkedin.com/in/ritesh-rai-3741ba178",
     email: "riteshrai447@gmail.com",
   },
   {
     name: "Samarth Agrawal",
-    description:
-      "This is basic card with image on top, title, description and button.",
+    description: "Founding Member, CODE",
     img: Samarth,
     detailedText:
-      "This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.",
+      "A Computer Science and Engineering branch 2021 batch passout, having great management skills. He is also a great coder and photoshop designer.",
     fbLink: "https://www.facebook.com/samarthagarwal1414/",
     lnkdinLink: "https://www.linkedin.com/in/samarth-agarwal-545429133/",
     email: "samarthagarwal1414@gmail.com",
   },
   {
     name: "Rajat Saxena",
-    description:
-      "This is basic card with image on top, title, description and button.",
+    description: "Founding Member, CODE",
     img: Rajat,
     detailedText:
-      "This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.",
+      "A scholar from Electronics and Communication Engineering branch, 2021 batch passout. He is an amazing web developer and graphic designer.",
     fbLink: "https://www.facebook.com/rajat.saxena.90410",
     lnkdinLink: "https://www.linkedin.com/in/rajat-saxena-54509a15b/",
     email: "saxena1975sanjeev@gmail.com",
   },
 ];
-const TeamMember = ({ name, description, img, detailedText,fbLink,lnkdinLink,email }) => {
+const TeamMember = ({
+  name,
+  description,
+  img,
+  detailedText,
+  fbLink,
+  lnkdinLink,
+  email,
+}) => {
   return (
     <div
       className="col-xs-12 col-sm-6 col-md-4"
@@ -53,14 +59,22 @@ const TeamMember = ({ name, description, img, detailedText,fbLink,lnkdinLink,ema
             <div className="card" style={{ marginBottom: "0!important" }}>
               <div className="card-body text-center">
                 <p>
-                  <img className="img-fluid" src={img} alt="card image" />
+                  <LazyLoadImage
+                    src={img}
+                    className="img-fluid"
+                    alt="card image"
+                  />
+                  {/* <img className="img-fluid" src={img} alt="" /> */}
                 </p>
                 <h4 className="card-title" style={{ fontWeight: 600 }}>
                   {name}
                 </h4>
                 <p className="card-text">{description}</p>
                 <Link to="#" className="flip">
-                  <img src="https://img.icons8.com/android/24/000000/right3.png" alt=""/>
+                  <img
+                    src="https://img.icons8.com/android/24/000000/right3.png"
+                    alt=""
+                  />
                 </Link>
               </div>
             </div>
@@ -72,23 +86,17 @@ const TeamMember = ({ name, description, img, detailedText,fbLink,lnkdinLink,ema
                   {name}
                 </h4>
                 <p className="card-text">{detailedText}</p>
-                {/* <ul className="list-inline">
+                <ul className="list-inline">
                   <li className="list-inline-item">
                     <a
                       className="social-icon text-xs-center"
                       target="_blank"
                       href={fbLink}
                     >
-                      <img src="https://img.icons8.com/fluent/32/000000/facebook-new.png" alt="" />
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a
-                      className="social-icon text-xs-center"
-                      target="_blank"
-                      href="#"
-                    >
-                      <img src="https://img.icons8.com/color/32/000000/twitter-circled.png" alt="" />
+                      <LazyLoadImage
+                        src="https://img.icons8.com/fluent/32/000000/facebook-new.png"
+                        alt=""
+                      />
                     </a>
                   </li>
                   <li className="list-inline-item">
@@ -97,20 +105,26 @@ const TeamMember = ({ name, description, img, detailedText,fbLink,lnkdinLink,ema
                       target="_blank"
                       href={lnkdinLink}
                     >
-                      <img src="https://img.icons8.com/fluent/32/000000/linkedin.png" alt="" />
+                      <LazyLoadImage
+                        src="https://img.icons8.com/fluent/32/000000/linkedin.png"
+                        alt=""
+                      />
                     </a>
                   </li>
                   <li className="list-inline-item">
                     <a
                       className="social-icon text-xs-center"
                       target="_blank"
-                      href="#"
+                      href={"mailto:" + email}
                     >
-                      <img src="https://img.icons8.com/fluent/32/000000/instagram-new.png" alt="" />
+                      <LazyLoadImage
+                        src="https://img.icons8.com/color/48/000000/gmail--v1.png"
+                        alt=""
+                        style={{ width: "2rem", height: "2rem" }}
+                      />
                     </a>
                   </li>
                 </ul>
-               */}
               </div>
             </div>
           </div>
