e8d81bc65c5ca0baa0f90f0c6717f8b9f29676cb
Sooraj Shukla <soorajshukla974@gmail.com>
2021-07-12 09:16:34 +0530

    give link to home page at auth components

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 const NavbarForLogin = () => {
   return (
     <React.Fragment>
-      <nav className="nav-bar">
-        <img
-          className="code-img"
-          src="https://club-of-developers.s3.ap-south-1.amazonaws.com/codeLogo.png"
-          alt=""
-        />
-      </nav>
+      <Link to="/">
+        <nav className="nav-bar">
+          <img
+            className="code-img"
+            src="https://club-of-developers.s3.ap-south-1.amazonaws.com/codeLogo.png"
+            alt=""
+          />
+        </nav>
+      </Link>
     </React.Fragment>
   );
 };
