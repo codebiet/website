9b28c9f63a8f8763323a0b74e8977063cf99832a
Sooraj Shukla <soorajshukla974@gmail.com>
2021-07-12 07:40:57 +0530

    redirect to home page after logout

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -1,12 +1,12 @@
-import React, {useEffect, useContext} from "react";
-import {AuthContext} from "../state/Store";
-import {logOutUser} from "../state/auth/authActions";
-import {Redirect} from "react-router-dom";
+import React, { useEffect, useContext } from "react";
+import { AuthContext } from "../state/Store";
+import { logOutUser } from "../state/auth/authActions";
+import { Redirect } from "react-router-dom";
 const Logout = () => {
-    const auth = useContext(AuthContext);
-    useEffect(() => {
-      logOutUser(auth.dispatch);
-    },[]);
-    return <Redirect to="/login" />;
-  };
+  const auth = useContext(AuthContext);
+  useEffect(() => {
+    logOutUser(auth.dispatch);
+  }, []);
+  return <Redirect to="/" />;
+};
 export default Logout;
