fb44dbecc5598c39898b38950fb5083fe61e28fa
Sooraj Shukla <soorajshukla974@gmail.com>
2021-07-17 15:40:52 +0530

    update loader for index, add keywords, update title

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 const App = lazy(() => import("./components/App/App.js"));
+import Loader from "./components/Loader/Loader";
 ReactDOM.render(
   <Suspense fallback={<h1>Loading...</h1>}>
     <App />
