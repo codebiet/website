7376432223daed18ec958f557c0b06003e86842e
Sooraj Shukla <soorajshukla974@gmail.com>
2021-07-17 15:40:52 +0530

    update initial loader, add keywords and update title

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 const App = lazy(() => import("./components/App/App.js"));
+import Loader from "./components/Loader/Loader";
 ReactDOM.render(
-  <Suspense fallback={<h1>Loading...</h1>}>
+  <Suspense fallback={<Loader />}>
     <App />
   </Suspense>,
   document.getElementById("app")
